b319ffaf042138d88a6dae86c40070989172f2d7
Andre Ferreira <delcferreira@gmail.com>
2021-12-19 18:33:13 -0300

    :tada: Begin project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Easy git rebase
+
+### How to setting
+
+You need put it in your linux alias to run, I sugetion you added it, with name "rebase"
+
+### How to run
+
+```bash
+    rebase -u develop master
+```
+
+### Get help
+
+```bash
+    rebase -h
+```
---
+++ main.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+if [ $1 = "-u" ]
+then 
+    git checkout $3
+    git pull
+
+    git checkout $2
+    git pull
+
+    git checkout $3
+    git rebase $2
+
+    git checkout $2
+fi
+
+if [ $1 = "-v" ]
+then
+    cat ./_version.txt
+fi
+
+if [ $1 = '-h' ]
+then
+    cat ./_help.txt
+fi
+
+#---- test 7
